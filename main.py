2e86d8a7b7b869551ff3274454b617cee6b884b9
mab1k <piv0004@yandex.ru>
2025-05-07 19:52:46 +0300

    ADD Pytest and Allure

--- main.py
+++ main.py
@@ -1,21 +1,55 @@
-from time import sleep
-
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.expected_conditions import element_to_be_clickable
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import pytest
+import allure
 
-service = Service("chromedriver.exe")
-driver = webdriver.Chrome(service=service)
+PATH_CHROMEDRIVER = "chromedriver.exe"
+EXPECTED_URL = "https://www.iana.org/help/example-domains"
+EXPECTED_TEXT = "More information"
 
-driver.maximize_window()
 
-driver.get("https://example.com")
+@pytest.fixture
+def init_driver():
+    with allure.step("Init webdriver in headless mode"):
+        service = Service(PATH_CHROMEDRIVER)
 
-# Так как в Selenium в CSS селекторе не поддерживается конструкции такого вида :contains('text'), можем использовать XPath
-element = driver.find_element(By.XPATH, "//a[contains(text(), 'More information')]")
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
 
-# Вот как выглядит это с помощью CSS селектора
-# element = driver.find_element(By.CSS_SELECTOR, 'a[href*="iana.org"]')
+        driver = webdriver.Chrome(service=service, options=chrome_options)
+        driver.maximize_window()
 
-element.click()
+        yield driver
+
+        driver.quit()
+
+
+@allure.story("Test example")
+def tests_example(init_driver):
+    with allure.step("Get url https://example.com"):
+        init_driver.get("https://example.com")
+
+    try:
+        # Так как в Selenium в CSS селекторе не поддерживается конструкции такого вида :contains('text'), можем использовать XPath
+        with allure.step("Get el with text contains 'More information'"):
+            element = WebDriverWait(init_driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, f"//a[contains(text(), '{EXPECTED_TEXT}')]"))
+            )
+        # Вот как выглядит это с помощью CSS селектора
+        # element = init_driver.find_element(By.CSS_SELECTOR, 'a[href*="iana.org"]')
+        with allure.step("Click el"):
+            element.click()
+
+        assert init_driver.current_url == EXPECTED_URL
+
+    except Exception as e:
+        with allure.step("Test failed"):
+            allure.attach(init_driver.get_screenshot_as_png(), name="failure-screenshot",
+                          attachment_type=allure.attachment_type.PNG)
+            pytest.fail(f"Test failed: {str(e)}")
